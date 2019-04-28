bd261ff10e983d26e00637813fe3b5250eea6c25
zloblya2018 <zloblya2018@mail.ru>
2019-04-29 00:00:34 +0300

    1.3. JavaDoc. Code convention[#134729]

---
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -0,0 +1,19 @@
+package ru.job4j.calculate;
+
+/**
+*Класс калькулятор
+*@author Maksim Belyakov (belyak1313@bk.ru)
+*@since 28.04.2019
+*@version 1
+*/
+
+public class Calculate {
+    /**
+    *Метод Main
+    *@param args - args
+    *@return String
+    */
+    public static void main(String[] args) {
+        System.out.println("Hello world!!!");
+    }
+}
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Maksim Belyakov (belyak1313@bk.ru)
+* @version $Id$
+* @since 28.04.2019
+*/
+package ru.job4j.calculate;
