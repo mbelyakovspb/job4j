47c2bd063aec1b4d7f1e9f9f2e8270736e086218
zloblya2018 <zloblya2018@mail.ru>
2019-04-29 22:58:46 +0300

    1.3.1 Maven Plugins[#134725]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,17 +1,17 @@
 package ru.job4j.calculate;
 
 /**
-*Класс калькулятор
-*@author Maksim Belyakov (belyak1313@bk.ru)
-*@since 28.04.2019
-*@version 1
+* Класс калькулятор
+* @author Maksim Belyakov (belyak1313@bk.ru)
+* @since 28.04.2019
+* @version 1
 */
 
 public class Calculate {
     /**
-    *Метод Main
-    *@param args - args
-    *@return String
+    * Метод Main
+    * @param args
+    * @return String
     */
     public static void main(String[] args) {
         System.out.println("Hello world!!!");
