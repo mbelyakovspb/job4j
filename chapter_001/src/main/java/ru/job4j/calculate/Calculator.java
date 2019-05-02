ab2a7e04c0b8ca14cfd2fcd3d0c2f2ddd260fab7
mbelyakovspb <belyak1313@bk.ru>
2019-05-02 15:14:10 +0300

    3.1. Элементарный калькулятор.[#134719]

---
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculator.java
@@ -0,0 +1,17 @@
+package ru.job4j.calculate;
+
+public class Calculator {
+    public double add(double first, double second) {
+        return first + second;
+    }
+    public double sub(double first, double second) {
+        return second - first;
+    }
+    public double multipl(double first, double second) {
+        return first * second;
+    }
+    public double div(double first, double second) {
+        return first / second;
+    }
+
+}
---
+++ chapter_001/src/test/java/ru/job4j/calculate/CalculatorTest.java
@@ -0,0 +1,42 @@
+package ru.job4j.calculate;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+
+    @Test
+    public void whenTwoPlusTwoThenFour() {
+        Calculator calculator = new Calculator();
+        double result = calculator.add(2D, 2D);
+        double expected = 4D;
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenTwoSubstructTwoThenZero() {
+        Calculator calculator = new Calculator();
+        double result = calculator.sub(2D, 2D);
+        double expected = 0D;
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenThreeMultiplyThreeThenNine() {
+        Calculator calculator = new Calculator();
+        double result = calculator.multipl(3D, 3D);
+        double expected = 9D;
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenTwoDivideTwoThenOne() {
+        Calculator calculator = new Calculator();
+        double result = calculator.div(2D, 2D);
+        double expected = 1D;
+        assertThat(result, is(expected));
+
+    }
+}
