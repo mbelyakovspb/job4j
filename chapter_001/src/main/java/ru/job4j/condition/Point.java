f36571ec7cf42ec47b1df205b8138e4ec2c36069
mbelyakovspb <belyak1313@bk.ru>
2019-06-03 12:41:11 +0300

    3. Расстояние между точками в трехмерном пространстве.[#134818]

--- chapter_001/src/main/java/ru/job4j/condition/Point.java
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -1,4 +1,6 @@
 package ru.job4j.condition;
+
+import static java.lang.Math.*;
 /**
  * Класс Point
  * @author Maksim Belyakov (belyak1313@bk.ru)
@@ -15,6 +17,10 @@ public class Point {
      */
     private int y;
     /**
+     * Это поле объекта. Оно доступно только конкретному объекту.
+     */
+    private int z;
+    /**
      * Конструктор, который принимает начальное состояние объекта "точка"
      * @param first координата x
      * @param second координата y
@@ -24,14 +30,42 @@ public class Point {
         this.y = second;
     }
     /**
+     * Конструктор, который принимает начальное состояние объекта "точка"
+     * @param first координата x
+     * @param second координата y
+     * @param third координата z
+     */
+    public Point(int first, int second, int third) {
+        this.x = first;
+        this.y = second;
+        this.z = third;
+    }
+    /**
      * Метод distance вычисляет расстояния между точками
      * @param that - координаты точки на графике
      * @return расстояние между точками
      */
     public double distance(Point that) {
-        return Math.sqrt(Math.pow(this.x - that.x, 2) + Math.pow(this.y - that.y, 2));
+        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
     }
+    /**
+     * Метод distance3d вычисляет расстояния между точками в трехмерном пространстве
+     * @param that - координаты точки на графике
+     * @return расстояние между точками
+     */
+    public double distance3d(Point that) {
+        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2) + pow(this.z - that.z, 2));
+    }
+    /**
+     * Метод info выводит на экран координаты 2 точек
+     */
     public void info() {
         System.out.println(String.format("Point[%s, %s]", this.x, this.y));
+    }
+    /**
+     * Метод info3d выводит на экран координаты 3 точек
+     */
+    public void info3d() {
+        System.out.println(String.format("Point[%s, %s, %s]", this.x, this.y, this.z));
     }
 }
--- chapter_001/src/test/java/ru/job4j/condition/PointTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/PointTest.java
@@ -18,6 +18,16 @@ public class PointTest {
         assertThat(result, is(10D));
     }
     @Test
+    public void whenX1Equal1Y1Equal2Z1Equal3X2EqualMinus7Y2EqualMinus2Z2Equal4Then9() {
+        Point first = new Point(1, 2, 3);
+        Point second = new Point(-7, -2, 4);
+        double result = first.distance3d(second);
+        first.info3d();
+        second.info3d();
+        System.out.println(String.format("Result is %s", result));
+        assertThat(result, is(9D));
+    }
+    @Test
     public void whenCheckItSelf() {
         Point point = new Point(0, 0);
         double result = point.distance(point);
@@ -29,5 +39,9 @@ public class PointTest {
         first.info();
         Point second = new Point(2, 2);
         second.info();
+        Point first3d = new Point(1, 2, 3);
+        first3d.info3d();
+        Point second3d = new Point(-7, -2, 3);
+        second3d.info3d();
     }
 }
